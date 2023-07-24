688725c6b513422f3efe1d95b727adee0c7f49fb
木村直花 <n.kimura@iimon.co.jp>
2023-07-24 18:15:24 +0900

    指定の犬種の画像が出るように修正

--- test1/main.js
+++ test1/main.js
@@ -1,31 +1,16 @@
-const button = document.getElementById('submit');
-const breed = document.getElementById('breed-input');
-const image = document.getElementById('img');
+const button = document.getElementById("submit");
+const breed = document.getElementById("breed-input");
+const image = document.getElementById("img");
 
-let url="";
-
-breed.addEventListener('change', ()=>{
-  console.log(breed.value);
-  url = 'https://dog.ceo/api/breeds/image/random'
-
-  url = url + '?breed=' + breed.value;
+button.addEventListener("click", () => {
+  let url = "https://dog.ceo/api/breed/" + breed.value + "/images/random";
   console.log(url);
-})
-console.log(url)
-
-button.addEventListener('click', () => {
-
-
-fetch(url)
-
-.then((response)=>{
-  return response.json()
-
-})
-
-.then((data)=>{
-  console.log('成功',data);
-  image.src = data['message'];
-})
-
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log("成功", data);
+      image.src = data["message"];
+    });
 });
